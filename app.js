14003aefb8a072775fb8958fac6dffcf9ff87c80
Eric <avilaeric295@gmail.com>
2022-01-18 19:12:15 -0600

    Introduccion a javascript

---
+++ app.js
@@ -0,0 +1,155 @@
+/*var nombre = 'Eric';
+console.log(nombre);
+console.log(typeof(nombre));
+
+var edad = 30;
+console.log(edad);
+console.log(typeof(edad));
+
+var sueldo = 2000;
+console.log(sueldo);
+console.log(typeof(sueldo));
+*****************************/
+
+/*******Operadores matemáticos*********/
+/*var edadAna, edadMaria, diferenciaEdad;
+var sumaEdades, yearAna, yearMaria, yearActual;
+
+edadAna = 34;
+edadMaria = 28;
+yearActual = 2021;
+
+diferenciaEdad = edadAna - edadMaria;
+sumaEdades = edadAna + edadMaria;
+
+yearAna = yearActual - edadAna;
+yearMaria = yearActual - edadMaria;
+
+console.log(diferenciaEdad);
+console.log(sumaEdades);
+console.log('Año en que nació Ana' + yearAna);
+console.log('Año en que nació Maria' + yearMaria);
+console.log(yearActual * 5);
+console.log(yearActual / 2);*/
+
+/**Operadores logicos, unarios y de asiganción**/
+//var edadAna, edadMaria, diferenciaEdad;
+//edadAna = 34;
+//edadMaria = 28;
+
+//var mayorAna = !(edadAna == edadMaria);
+//console.log(mayorAna);
+
+//Unarios, ++Incremento, --Decremento
+//edadAna++;
+/*console.log(++edadAna);
+console.log(edadAna);
+
+//Asiganción, +=, -=, *=, /=, %= operador modulo
+var a = 12;
+var b = 5;
+
+var c = a % 5; //Residuo o resto de una división
+console.log(c);
+a += b;
+console.log(a);*/
+
+/*****Sentencia if, else *****/
+/*var nombre = 'Eric';
+var esCasado = false;
+
+if(esCasado == true){
+    console.log(nombre + ' es casado.');
+}else{
+    console.log(nombre + ' es soltero');
+}*/
+
+/*var nombre = 'Eric';
+var edad = 100;
+
+//edad < 12 es un niño
+//edad > 11, es < 18, es un adolescente
+//edad > 17, es < 60, es un adulto
+//edad > 60 es un anciano
+
+if(edad < 12){
+    console.log(nombre + ' es un niño');
+}else if((edad > 11) && (edad < 18)){
+    console.log(nombre + ' es un adolescente');
+}else if((edad > 17) && (edad < 60)){
+    console.log(nombre + ' es un adulto');
+}else{
+    console.log(nombre + ' es un anciano');
+}*/
+
+/***Sentencia Switch***/
+
+/*var mes = 10;
+
+switch(mes){
+    case 1:
+        console.log('Enero');
+        break;
+    case 2:
+        console.log('Febrero');
+        break;
+    case 3:
+        console.log('Marzo');
+        break;
+    case 4:
+        console.log('Abril');
+        break;
+    case 5:
+        console.log('Mayo');
+        break;
+    case 6:
+        console.log('Junio');
+        break;
+    case 7:
+        console.log('Julio');
+        break;
+    case 8:
+        console.log('Agosto');
+        break;
+    case 9:
+        console.log('Septiembre');
+        break;
+    case 10:
+        console.log('Octubre');
+        break;
+    case 11:
+        console.log('Noviembre');
+        break;
+    case 12:
+        console.log('Diciembre');
+        break;
+    default:
+        console.log('Mes no encontrado');
+}*/
+
+/**Sentencia For**/
+/*for(var i=1; i<=10; i++){
+    console.log(i);
+}
+for(var i=1; i<=10; i+=2){
+    console.log(i);
+}
+for(var i=10; i>=1; i--){
+    console.log(i);
+}*/
+
+/***Sentencia While***/
+/*var i = 1;
+while (i <= 10){
+    console.log(i);
+    i++;
+}
+console.log(i);*/
+
+/***Sentencia Do while***/
+/*var i = 11;
+do{
+    console.log(i);
+    //i++;
+}while(i <= 10);
+console.log(i);*/
---
+++ funciones.js
@@ -0,0 +1,69 @@
+/*function mensaje(msj){
+    console.log(msj);
+}
+
+var msj = 'Mensaje como argumento de la función';
+mensaje(msj);
+
+function mensaje(){
+    return 'Mensaje como devolución de la función';
+}
+
+var msj = mensaje();
+console.log(msj);*/
+
+/*function sumar(a, b, c=3){
+    return a + b + c;
+}
+
+var result = sumar(4, 5, 8);
+var resul1 = sumar(3, 7);
+console.log(result);
+console.log(resul1);*/
+
+var factorial = function(n){
+    if((n == 0) || (n == 1))
+    return 1;
+    
+    else
+    return(n * factorial(n - 1));
+}
+
+/**Arreglos**/
+var nombres = ['Eric', 'Carlos', 'Ana', 'Luisa'];
+var vegetales = new Array('Tomate', 'Lechuga', 'Zanahoria', 'Elote');
+
+/*console.log(nombres[3]);
+console.log(vegetales[1]);
+
+nombres[0] = 'Rocky';
+vegetales[2] = 'Chile';
+
+console.log(nombres[0]);
+console.log(vegetales[2]);*/
+
+/*console.log(nombres.length);
+for(var i = 0; i <= nombres.length -1; i++){
+    console.log(nombres[i]);
+}
+
+vegetales.forEach(function(elemento, indice){
+    console.log(elemento, indice);
+})*/
+
+console.log(nombres);
+nombres.push('Pedro');
+console.log(nombres);
+nombres.unshift('lili');
+console.log(nombres);
+nombres.pop();
+console.log(nombres);
+nombres.shift();
+console.log(nombres);
+
+var pos = nombres.indexOf('Carlos');
+console.log(pos);
+
+nombres.splice(1, 1);
+
+console.log(nombres);
